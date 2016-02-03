ebb509c63633c4f63e5f5432ac95d7ec4b8b36d9
Tom <t.m.lemanski@googlemail.com>
2016-02-03 14:58:09 +0000

    Redirect added

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ if ('development' == app.get('env')) {
 
 app.get("/", routes.index); // Home route
 app.get("/new/:url", routes.urlshortener); // Time query route
+app.get("/:path", routes.redirect);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/routes.js
+++ routes/routes.js
@@ -10,43 +10,64 @@ var URL = require("../app/models/url");
  * GET home page.
  */
 //exports.module = function(app, mongoose, mongo, configDB){
-    exports.index = function(req, res){
-        res.render('index', {
-            title: "URL Shortener Microservice",
-            author: "ch4tml"
-        });
-    };
-    /*
-     * GET time request and return JSON object
-     */
-    exports.urlshortener = function(req, res){
-        //var short_url = "https://ch4tml-url-ms.herokuapp.com/" + randomstring.generate(7);
-        var stringGenerated = randomstring.generate(7);
-        // If all well, writehead 200 with mimetype JSON*/
-        res.writeHead(200, {"Content-Type" : "application/json"});
-        // Create json object to return to user
-        
-        var enteredURL = new URL();
-        
-        enteredURL.original_url = req.params.url;
-        enteredURL.short_url = "https://ch4tml-url-ms.herokuapp.com/" + stringGenerated;
-        enteredURL.stringGenerated = stringGenerated;
-        
+exports.index = function(req, res){
+    res.render('index', {
+        title: "URL Shortener Microservice",
+        author: "ch4tml"
+    });
+};
+/*
+ * GET time request and return JSON object
+ */
+exports.urlshortener = function(req, res){
+    //var short_url = "https://ch4tml-url-ms.herokuapp.com/" + randomstring.generate(7);
+    var stringGenerated = randomstring.generate(7);
+    // If all well, writehead 200 with mimetype JSON*/
+    res.writeHead(200, {"Content-Type" : "application/json"});
+    // Create json object to return to user
+    
+    var enteredURL = new URL();
+    
+    enteredURL.original_url = req.params.url;
+    enteredURL.short_url = "https://ch4tml-url-ms.herokuapp.com/" + stringGenerated;
+    enteredURL.stringGenerated = stringGenerated;
+    
+    console.log(enteredURL);
+    
+    enteredURL.save(function(err){
+        if(err) return console.error(err);
+        console.log("URL saved successfully");
         console.log(enteredURL);
-        
-        enteredURL.save(function(err){
-            if(err) return console.error(err);
-            console.log("URL saved successfully");
-            console.log(enteredURL);
-        });
+    });
 
-        db.on("error", console.error.bind(console, "Connection error: "));
-        
-        URL.find({stringGenerated: "Xokbi66"}, function(err, doc){
-            if(err) throw err;
-            doc = JSON.stringify(doc);
-            res.end(doc);
-        });
-        //var data = JSON.stringify(enteredURL);
-        //res.end(data);
+    db.on("error", console.error.bind(console, "Connection error: "));
+    /*
+    URL.find({original_url: req.params.url}, function(err, doc){
+        if(err) throw err;
+        doc = JSON.stringify(doc);
+        res.end(doc);
+    });*/
+    //var data = JSON.stringify(enteredURL);
+    //res.end(data);
+    
+    var json = {
+        "original_url": req.params.url,
+        "short_url": enteredURL.short_url
     };
+    var data = JSON.stringify(json);
+    res.end(data);
+};
+
+exports.redirect = function(req, res){
+    db.on("error", console.error.bind(console, "Connection error: "));
+    var redirectUrl;
+    URL.findOne({stringGenerated: req.params.path}, function(err, doc){
+        if(err) throw err;
+        console.log(doc["original_url"]);
+        redirectUrl = doc["original_url"];
+        console.log(redirectUrl);
+        //res.end(doc["original_url"]);
+        console.log("XKCD: " + redirectUrl);
+        res.redirect("http://" + redirectUrl);
+    });
+};
