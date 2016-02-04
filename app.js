f8004c77836c0b78128134919538835f2429e8ee
Tom <t.m.lemanski@googlemail.com>
2016-02-04 14:08:39 +0000

    Refactored code, added comments

--- app.js
+++ app.js
@@ -6,35 +6,34 @@ var routes = require("./routes/routes");
 var bodyParser = require('body-parser');
 var path = require("path");
 var http = require("http");
+
+// Database setup
 var mongo = require("mongodb").MongoClient;
 var mongoose = require("mongoose");
 var configDB = require("./config/database.js");
 mongoose.connect(configDB.url);
-//var url = "mongodb://localhost:27017/paths";
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// all environments
+// All environments
 app.set('port', process.env.PORT || 8080);
 app.use(express.favicon());
 // Logs actions and requests when server running
 app.use(express.logger('dev'));
-//app.use(express.bodyParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get("/", routes.index); // Home route
 // Need to include "http://" in query. * - url wildcard query
-//app.get("/new/http://:url*", routes.urlshortener);
 app.get("/new/*", routes.urlshortener);
 // Allows user to enter short url path to redirect
 app.get("/:path", routes.redirect);
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 "use strict";
+// Package that generates random string
 var randomstring = require("randomstring");
+// Database setup
 var mongo = require("mongodb").MongoClient;
 var mongoose = require("mongoose");
 var configDB = require("../config/database.js");
@@ -12,7 +14,6 @@ exports.index = function(req, res){
     res.render('index', {
         title: "URL Shortener Microservice",
         author: "ch4tml"
-        //heroku: "https://ch4tml-url-ms.herokuapp.com/"
     });
 };
 
@@ -20,9 +21,11 @@ exports.index = function(req, res){
 exports.urlshortener = function(req, res){
     var original_url = convertUrl(req.params[0]);
     function validateUrl(url){
+        // Validation url written by Elsner, available at http://regexr.com/39nr7. Validates co.uk
         var re_validateUrl = new RegExp(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/ig);
         return re_validateUrl.test(url);
     }
+    // Converts url without http:// into a url with http:// - standardises the original_url saved to DB
     function convertUrl(url){
         var re = new RegExp(/^http:\/\//i);
         return re.test(req.params[0]) ? req.params[0] : "http://" + req.params[0];
@@ -37,22 +40,24 @@ exports.urlshortener = function(req, res){
         // Checks if URL has already been shortened in the database
          URL.findOne({original_url: convertUrl(req.params[0])}, function(err, doc){
             if(err) throw err;
-            // If a URL has already been shortened, return a json object showing the original url and short url - prevents repetition of entries in DB
+            // If a URL has already been shortened, return a json object showing the original_url and short_url - prevents repetition of entries in DB
             else if(doc){
-                // If all well, writehead 200 with mimetype JSON*/
+                // If all well, writehead 200 with mimetype JSON
                 res.writeHead(200, {"Content-Type" : "application/json"});
+                console.log("Found previous entry for URL"); // Logging
+                console.log("Returning existing entry"); // Logging
                 res.end(JSON.stringify({
                     original_url: doc.original_url,
                     short_url: doc.short_url,
                 }));
             }
-            // If URL is valid and not currently in database, then create new short url
+            // If URL is valid and not currently in database, then generate new short_url and json object
             else{
                 // Generate random string of length characters 7
                 var stringGenerated = randomstring.generate(7);
                 // If all well, writehead 200 with mimetype JSON*/
                 res.writeHead(200, {"Content-Type" : "application/json"});
-        
+                // Create new url based on URL Schema
                 var enteredURL = new URL();
                 enteredURL.original_url = original_url;
                 enteredURL.short_url = "https://ch4tml-url-ms.herokuapp.com/" + stringGenerated;
@@ -60,7 +65,7 @@ exports.urlshortener = function(req, res){
             
                 enteredURL.save(function(err){
                     if(err) return console.error(err);
-                    console.log("URL saved successfully");
+                    console.log("URL saved successfully"); // Logging
                 });
             
                 db.on("error", console.error.bind(console, "Connection error: "));
@@ -82,6 +87,11 @@ exports.redirect = function(req, res){
     db.on("error", console.error.bind(console, "Connection error: "));
     URL.findOne({stringGenerated: req.params.path}, function(err, doc){
         if(err) throw err;
-        res.redirect(doc["original_url"]);
+        if(doc)
+            res.redirect(doc["original_url"]);
+        else
+            res.end(JSON.stringify({
+                "Error": "Invalid URL entered"
+            }));
     });
 };
